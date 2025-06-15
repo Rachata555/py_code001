02024d8ea3896d792657add7e949c1fd4082dd6b
DESKTOP-R9714V9\student <s6603130121125@email.kmutnb.ac.th>
2025-06-15 15:34:47 +0700

    add damage

--- NPC.py
+++ NPC.py
@@ -8,9 +8,22 @@ class NPC:
         print(f"HP: {self.HP}")
         print(f"Mana: {self.Mana}")
         print("-"*20)
+        
+    def damage(self,damage):
+        if self.Mana <= 0:
+            print("not enough Mana")
+            return
+        self.HP -= damage
+        self.Mana -= damage
+        print(f"{self.name} HP got -{damage}")
 
 n1 = NPC("Alice",100,150)
 n2 = NPC("Bob",100,250)
 
 n1.describe()
+n1.damage(10)
+n1.damage(10)
+n1.describe()
+
+
 n2.describe()
