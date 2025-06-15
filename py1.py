1f04ad566b75d43e3a70085c1548f1ee9fe9cfb5
DESKTOP-R9714V9\student <s6603130121125@email.kmutnb.ac.th>
2025-06-15 11:33:07 +0700

    add salary

--- py1.py
+++ py1.py
@@ -1,24 +1,28 @@
 class Student:
-    def __init__(self, firstname,lastname, age, courses,money):
+    student_count = 0
+    def __init__(self, firstname,lastname, age, courses,salary):
         self.firstname = firstname
         self.lastname = lastname
         self.age = age
         self.courses = courses
-        self.money = money
+        self.money = 0
+        self.salary = salary
+        Student.student_count = 0
         
     def get_fullname(self):
-        return f"{self.firstname}{self.lastname}"
+        return f"{self.firstname} {self.lastname}"
 
     def display_info(self):
         print("Name:", self.get_fullname())
         print("Age:", self.age)
         print("Courses:", ", ".join(self.courses))
         print("money", self.money)
+        print("salary", self.salary)
         print("-" * 20)
 
 
-student1 = Student("Alice", "Boder", 20, ["Math", "English"],90)
-student2 = Student("Bob","Ba", 22, ["Physics", "History"],10)
+student1 = Student("Alice", "Boder", 20, ["Math", "English"],15000)
+student2 = Student("Bob","Ba", 22, ["Physics", "History"],20000)
 
 student1.display_info()
 student2.display_info()
