af724ac60e70126bff9f46b7c1a456df95df3cb0
DESKTOP-R9714V9\student <s6603130121125@email.kmutnb.ac.th>
2025-06-15 15:45:20 +0700

    add heal

--- NPC.py
+++ NPC.py
@@ -1,16 +1,23 @@
 class NPC:
-    def __init__(self,s_name,s_HP,s_Mana):
+    def __init__(self,s_name,HP,Mana):
         self.name =s_name
-        self.HP = s_HP
-        self.Mana = s_Mana
+        self.HP = HP
+        self.Mana = Mana
     def describe(self):
         print(f"name: {self.name}")
         print(f"HP: {self.HP}")
         print(f"Mana: {self.Mana}")
         print("-"*20)
+        
+    def heal(self,heal):
+        self.HP += heal
 
 n1 = NPC("Alice",100,150)
 n2 = NPC("Bob",100,250)
 
+
+n1.heal(20)
 n1.describe()
+
+n2.heal(30)
 n2.describe()
