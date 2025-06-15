81bdfe39fe772f6a2b8db638d0289cabf983f3d7
DESKTOP-R9714V9\student <s6603130121125@email.kmutnb.ac.th>
2025-06-15 11:05:14 +0700

    ah

--- py1.py
+++ py1.py
@@ -6,10 +6,11 @@ class Student:
         self.courses = courses
         self.money = money
         
-
+    def get_fullname(self):
+        return f"{self.firstname}{self.lastname}"
 
     def display_info(self):
-        print("Name:", self.firstname , self.lastname)
+        print("Name:", self.get_fullname())
         print("Age:", self.age)
         print("Courses:", ", ".join(self.courses))
         print("money", self.money)
@@ -21,3 +22,4 @@ student2 = Student("Bob","Ba", 22, ["Physics", "History"],10)
 
 student1.display_info()
 student2.display_info()
+print(student1.get_fullname())
